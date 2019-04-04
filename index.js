0c99d9e143fc08492c53cf9a7350adc0f38bef69
sagar <sagarrs293@gmail.com>
2019-04-04 12:00:04 +0530

    backend setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+var cors = require("cors")
+const {mongoose} = require("./config/database")
+
+const port = 3005
+
+const app = express()
+
+// "use" is to configure express middleware
+app.use(express.json())
+app.use(cors())
+
+app.get("/", function(req, res){
+    res.send("<h1>Welcome To New Blog</h1>")
+})
+
+app.listen(port, function(){
+    console.log("Listening on port", port)
+})
