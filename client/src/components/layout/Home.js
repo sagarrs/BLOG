e1bae70fe23d6865ada534ce59d382e77809c11e
sagar <sagarrs293@gmail.com>
2019-04-12 01:17:08 +0530

    css 1.1

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -6,11 +6,15 @@ class Home extends React.Component{
         const tabs = {
             color: 'grey',
           };
-          
+        const headFont = {
+            fontFamily: "Lucida Console",
+            fontWeight: "bold"
+        };
+
         return(
             <div>
                 <div className="container">
-                    <h1>Medium Clone</h1><br/>
+                    <h1 style={headFont}>Medium Clone</h1><br/>
                     <div className="text-center">
                         <Link to="/" style={tabs}>Home</Link> &nbsp; 
                         <Link to="/" style={tabs}>Culture</Link> &nbsp;
@@ -19,7 +23,10 @@ class Home extends React.Component{
                         <Link to="/" style={tabs}>Health</Link>
                     </div>
                     
-                    <div>
+                    <div className="row" style={{padding: 20}}>
+                        <div className="text-center">
+                            <img style={{width: 1050, height: 500}} src={'https://cathylu25.files.wordpress.com/2015/02/graffiti-art121.jpg'} />
+                        </div>
                     </div>
                 </div>
             </div>
