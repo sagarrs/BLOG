ffa38e1cccfeb6dff4545afe7ce6aa89927b6f2d
sagar <sagarrs293@gmail.com>
2019-04-15 11:03:26 +0530

    CRUD OPS 2.0

--- client/src/App.js
+++ client/src/App.js
@@ -45,20 +45,53 @@ import ShowStory from "./components/stories/ShowStory"
 import EditStory from "./components/stories/EditStory"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      // isAuthenticated: false
+      // not not of truthy value is true
+      // not not of empty string is false coz empty string is a falsy value
+      isAuthenticated: !!localStorage.getItem("token")
+    }
+  }
+
+  handleIsAuthenticated = (bool) => {
+    console.log(bool)
+    this.setState(() => ({
+      isAuthenticated: bool
+    }))
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="container">
             <div className="float-right">
-              <Link className="login-reg" to="/register">Register</Link> &nbsp;   
-              <Link className="login-reg" to="/login">Login</Link>
+              {
+                this.state.isAuthenticated && <Link className="login-reg" to="/login">Logout</Link>
+              }
+              {
+                !this.state.isAuthenticated && (
+                  <div>
+                    <Link className="login-reg" to="/register">Register</Link> &nbsp;  
+                    <Link className="login-reg" to="/login">Login</Link>
+                  </div>
+                )
+              }
+        
             </div>
 
           <Switch>
             <Route path="/" component={Home} exact={true}/>
             <Route path="/stories" component={Story} exact={true}/>
             <Route path="/register" component={Register} exact={true}/>
-            <Route path="/login" component={Login} exact={true}/>
+            {/* here we r using props coz the histoy method is not available in login
+            component in order to make it available we should pass props in render
+            which has the history obj. Without passing props as args if we redirect 
+            using "this.props.history.push" then it gives error */}
+            <Route path="/login" render={(props) => {
+              return (<Login {...props} handleIsAuthenticated={this.handleIsAuthenticated}/>)
+            }} exact={true}/>
             <Route path="/stories/new" component={NewStory} exact={true} />
             <Route path="/stories/edit/:id" component={EditStory} exact={true} />
             <Route path="/stories/:id" component={ShowStory} exact={true} />
