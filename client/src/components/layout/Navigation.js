cc66cc83bae03ba251ba77d22c93145fa5f91a32
sagar <sagarrs293@gmail.com>
2019-04-22 21:24:55 +0530

    react bootstrap navbar

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -1,67 +1,29 @@
-import React from 'react';
+import React from 'react'
+import {Nav, Navbar, Badge} from 'react-bootstrap'
 
-class NavDropdown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: false
-    };
-  }
-  showDropdown(e) {
-    e.preventDefault();
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-  render() {
-    const classDropdownMenu = 'dropdown-menu' + (this.state.isToggleOn ? ' show' : '')
-    return (
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false"
-          onClick={(e) => {this.showDropdown(e)}}>
-          {this.props.name}
-        </a>
-        <div className={classDropdownMenu} aria-labelledby="navbarDropdown">
-          {this.props.children}
-        </div>
-      </li>
+class Navigation extends React.Component{
+  render(){
+    return(
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Navbar.Brand href="/">Medium Clone</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              {/* <Nav.Link href="">Features</Nav.Link>
+              <Nav.Link href="">Pricing</Nav.Link> */}
+
+            </Nav>
+            <Nav>
+              <Nav.Link href="">Admin</Nav.Link>
+              <Nav.Link eventKey={2} href="">Login
+              <Badge pill variant="dark">1</Badge></Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      </div>
     )
   }
 }
 
-
-class Navigation extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Medium Clone</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            
-            
-              <NavDropdown name="Dropdown">
-                <a className="dropdown-item" href="/">Action</a>
-                <a className="dropdown-item" href="/">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="/">Something else here</a>
-              </NavDropdown>
-            
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-    )
-  }
-}
-
-export default Navigation;
-
-
+export default Navigation
