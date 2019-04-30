e143430ac0a66482b091780ad42d7d70bc280d07
sagar <sagarrs293@gmail.com>
2019-04-30 21:16:02 +0530

    edit page UI update 1.0

--- client/src/components/stories/ShowStory.js
+++ client/src/components/stories/ShowStory.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from '../../config/axios'
 import {Link} from 'react-router-dom'
+import '../../App.css'
 
 class ShowStory extends React.Component{
     constructor(){
@@ -10,12 +11,18 @@ class ShowStory extends React.Component{
         }
     }
 
-    handleDelete = () => {
+    handleDelete = (tagName) => {
         const id = this.props.match.params.id
+
+        const tagData = {
+            tagName: tagName.toString()
+        }
+        console.log(tagData)
+        console.log(id)
         // if you do just confirm without window it'll throw an error
         const confirm = window.confirm("Are u sure ?")
         if(confirm){
-            axios.delete(`/stories/${id}`)
+            axios.delete(`/stories/${id}`, { data: { tagName: tagName.toString() } })
                 .then((response) => {
                     console.log("deleted")
                     this.props.history.push("/stories")
@@ -30,10 +37,37 @@ class ShowStory extends React.Component{
         const id = this.props.match.params.id
         axios.get(`/stories/${id}`)
             .then((response) => {
-                console.log(response.data)
                 this.setState(() => ({
                     story: response.data
                 }))
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+            axios.get(`/tags`)
+            .then((response) => {
+                // console.log(response.data)
+
+                const x = response.data.filter(function(subarray) {
+                        return subarray.stories.filter(function(story) {
+                            return story == id
+                        })
+                    })
+                
+                // const result = response.data.map(function(ele){
+                //     console.log(ele)
+                //     return ele
+                // })
+                
+                // console.log("-------------------------------------")
+                // console.log(result)
+
+                //   scores.map(function(subarray) {
+                //     return subarray.map(function(number) {
+                //       return number * 3;
+                //     })
+                //   })
             })
             .catch((err) => {
                 console.log(err)
@@ -49,16 +83,22 @@ class ShowStory extends React.Component{
                     // })
                     
                 }
-                <p>Title - {this.state.story.body}</p>
-                <p>Body - {this.state.story.body}</p>
+                <p className="text-info">Title - {this.state.story.title}</p>
+                {/* <p>Body - {this.state.story.body}</p>
                 <p>Topic - {this.state.story.topicName}</p>
-                <p>Tags - {this.state.story.tagName}</p>
+                <p>Tags - {this.state.story.tagName}</p> */}
+                <blockquote>
+                    <p>{this.state.story.body}</p>
+                    <footer className="text-muted">Topic : {this.state.story.topicName}</footer>
+                    <footer className="text-muted">Tags - {this.state.story.tagName}</footer>
+                </blockquote>
                 <div>
-                    <img src={this.state.story.previewImageUrl} />
+                    {/* <img src={this.state.story.previewImageUrl} /> */}
+                    <img className="home-img" src={`http://localhost:3005/${this.state.story.previewImageUrl}`} /><br/><br/>
                 </div>
-                <input type="button" value="Delete" onClick={this.handleDelete}/><br/><br/>
-                <Link to={`/stories/edit/${this.props.match.params.id}`}>EDIT</Link><br/>
-                <Link to="/stories">BACK</Link>
+                <input type="button" className="btn btn-outline-success" value="Delete" onClick={() => {this.handleDelete(this.state.story.tagName)}}/>&nbsp;&nbsp;
+                <Link to={`/stories/edit/${this.props.match.params.id}`} className="btn btn-outline-success" >EDIT</Link><br/><br/>
+                <Link to="/stories" className="btn btn-outline-success">BACK</Link>
             </div>
         )
     }
